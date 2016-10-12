6cb5060833033259c8247e8b05e2d802661e2bb8
Michael Spreng <Michael Spreng>
2016-10-11 23:14:56 -0500

    no message

---
+++ app/src/main/java/com/allonsea/android/mysunshine/app/MainActivity.java
@@ -0,0 +1,50 @@
+package com.allonsea.android.mysunshine.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
